9babfe9208456aac8d8c50672cf384dd101d5854
Anri20 <88647934+Anri20@users.noreply.github.com>
2022-12-07 07:37:51 +0700

    index

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funny Memes</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+</head>
+
+<body>
+    <div class="container">
+
+    </div>
+</body>
+
+<script>
+    $(document).ready(function() {
+        $.post('').done(function(data){
+
+        })
+    })
+</script>
+
+</html>
